a84f39f9c6a720b0a55e5d2c5ac8a2ca3bb961d0
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-14 12:05:38 +0530

    1.1 : Accounts -> users model added

---
+++ accounts/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        
+        if not username:
+            raise ValueError("User must have an username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            password = password
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+
+
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
+    email = models.CharField(max_length=300, unique=True)
+    phone = models.CharField(max_length=50, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="users/profile/")
+    dob = models.DateField(null=True, blank=True)
+
+    GENDER_TYPE_LIST = [
+        ("Male", "Male"),
+        ("Female", "Female"),
+        ("Other", "Other"),
+    ]
+
+    gender = models.CharField(max_length=10, choices=GENDER_TYPE_LIST, null=True, blank=True)
+
+    #   required
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
+
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, add_label):
+        return True
