6c49679b42d8823ed543fd4dd7ced437c788b8d8
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-14 14:18:15 +0530

    1.4 : Accounts -> user can login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ from accounts.views import *
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register")
+    path("register/", RegisterView.as_view(), name="register"),
+    path("", SecretPageView.as_view(), name="test"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("logout/", LogoutUserView.as_view(), name="logout")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.forms import RegisterForm
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 
@@ -8,3 +11,20 @@ class RegisterView(CreateView):
     template_name = "accounts/register.html"
     form_class = RegisterForm
     success_url = reverse_lazy("accounts:register")
+
+
+class LoginUserView(LoginView):
+    template_name = "accounts/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect("/")
+        return super(LoginUserView, self).dispatch(request, *args, **kwargs)
+
+
+class LogoutUserView(LogoutView):
+    template_name = "accounts/register.html"
+
+
+class SecretPageView(LoginRequiredMixin, TemplateView):
+    template_name = "index.html"
